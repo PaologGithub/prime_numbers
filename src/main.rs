2b4934114d6577306d540154bd75f7ecce8681ff
PaologGithub <PaologGithub>
2026-09-23 20:31:34 +0200

    Added BitVec to reduces by 8 the size of each bool

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,61 @@
-use std::{sync::mpsc::{self, Receiver, Sender}, thread, time::Instant};
+use std::{sync::mpsc::{self, Receiver, Sender}, thread, time::Instant, usize};
 
 use progress_bar::{
     Color, Style, finalize_progress_bar, init_progress_bar, print_progress_bar_info, set_progress_bar_action, set_progress_bar_progress,
 };
 
+/// To store each `bool` as a single bit
+struct BitVec {
+    /// Each element stores 8 bool
+    data: Vec<u8>,
+    len: usize
+}
+
+impl BitVec {
+    pub fn new(len: usize) -> Self {
+        let byte_len = (len + 7) / 8;
+        Self {
+            data: vec![0u8; byte_len],
+            len
+        }
+    }
+
+    pub fn set(&mut self, index: usize, value: bool) {
+        if index > self.len {
+            panic!("index {} is greater than length {}", index, self.len);
+        }
+
+        let byte_index = index / 8;
+        let bit_index = index % 8;
+        if value {
+            self.data[byte_index] |= 1 << bit_index;
+        } else {
+            self.data[byte_index] &= !(1 << bit_index);
+        }
+    }
+
+    pub fn get(&self, index: usize) -> bool {
+        if index > self.len {
+            panic!("index {} is greater than length {}", index, self.len);
+        }
+
+        let byte_index = index / 8;
+        let bit_index = index % 8;
+        (self.data[byte_index] & (1 << bit_index)) != 0
+    }
+}
+
 fn calculate_thread(end: usize, sender: Sender<usize>) {
     // true if not prime
-    let mut is_composite = vec![false; end + 1];
+    let mut is_composite = BitVec::new(end + 1);
 
     for i in 2..=end {
-        if !is_composite[i] {
+        if !is_composite.get(i) {
             sender.send(i).unwrap();
 
             let mut j = i * i;
             while j <= end {
-                is_composite[j] = true;
+                is_composite.set(j, true);
                 j += i;
             }
         }
@@ -34,10 +75,10 @@ fn stdout_thread(end: usize, receiver: Receiver<usize>) -> Vec<usize> {
         
         if primes.len() % 1000 == 0 {
             let elapsed = start.elapsed().as_secs_f64();
-            let speed = prime as f64 / elapsed;
+            let speed = 1000.0 / elapsed;
             
             set_progress_bar_action(
-                &format!("{:.0} n/s", speed),
+                &format!("{:.0} p/s", speed),
                 Color::Blue,
                 Style::Bold
             );
@@ -60,7 +101,7 @@ fn stdout_thread(end: usize, receiver: Receiver<usize>) -> Vec<usize> {
 }
 
 fn main() {
-    let end: usize = 1_000_000;
+    let end: usize = 5_368_709_120;
 
     let (tx, rx): (Sender<usize>, Receiver<usize>) = mpsc::channel();
 
